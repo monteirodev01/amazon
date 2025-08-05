db90c90ccf47c0078f74148525539ff549971428
Paulo <paulodev2003@gmail.com>
2025-08-05 02:02:05 -0300

    Header criado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import css from './Header.module.css'
+import Logo from '../../assets/logo.png'
+import {CgShoppingBag} from 'react-icons/cg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={Logo} alt="" />
+        <span>amazon</span>
+      </div>
+
+      <div className={css.right}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sale</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+        <input type="text" placeholder='Search' className={css.search} />
+
+        <CgShoppingBag className={css.cart} />
+      </div>
+    </div>
   )
 }
 
